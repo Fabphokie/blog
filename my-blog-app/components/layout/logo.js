08313f62b7e1373ad63bd92d2190f94904fd6e9b
Fabphokie <mmaphokengsenne@gmail.com>
2023-09-21 15:02:04 +0200

    updates

---
+++ my-blog-app/components/layout/logo.js
@@ -0,0 +1,14 @@
+import classes from './logo.module.css'
+
+
+function Logo() {
+    return(
+      <div className={classes.logo}>
+         Phokie's Next blog
+
+      </div>
+    )
+
+}
+
+export default Logo;
---
+++ my-blog-app/pages/posts/[slug].js
@@ -0,0 +1,3 @@
+function PostDetailedPage() {}
+
+export default PostDetailedPage;
